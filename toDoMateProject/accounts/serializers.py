4a1da3a79528399a3c75a50817e89c79c9e898a4
sungmin <sungsung718@gmail.com>
2023-01-03 13:51:56 +0900

    Fix login & Add Google login

---
+++ toDoMateProject/accounts/serializers.py
@@ -0,0 +1,94 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from dj_rest_auth.serializers import LoginSerializer
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from .models import User
+
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls.base import reverse
+from allauth.account import app_settings
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import user_pk_to_url_str, user_username
+from allauth.utils import build_absolute_uri
+from dj_rest_auth.forms import AllAuthPasswordResetForm
+from dj_rest_auth.serializers import PasswordResetSerializer
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    username = None
+
+    def get_cleaned_data(self):
+        return {
+            'password1': self.validated_data.get('password1', ''),
+            'email': self.validated_data.get('email', ''),
+        }
+
+
+class CustomLoginSerializer(LoginSerializer):
+    username = None
+
+    def authenticate(self, **options):
+        return authenticate(self.context["request"], **options)
+
+    def validate(self, attrs):
+        username = None
+        email = attrs.get("email")
+        password = attrs.get("password")
+        if email and password:
+            user = self.get_auth_user(username, email, password)
+            if not user:
+                msg = "Unable to log in with provided credentials."
+                raise serializers.ValidationError({'Authorization error': msg}, code="authorization")
+        self.validate_email_verification_status(user)
+        attrs["user"] = user
+
+        return attrs
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'nickname', 'detail']
+
+
+class CustomAllAuthPasswordResetForm(AllAuthPasswordResetForm):
+    def save(self, request, **kwargs):
+        current_site = get_current_site(request)
+        email = self.cleaned_data['email']
+        token_generator = kwargs.get('token_generator',
+                                     default_token_generator)
+
+        for user in self.users:
+
+            temp_key = token_generator.make_token(user)
+
+            # save it to the password reset model
+            # password_reset = PasswordReset(user=user, temp_key=temp_key)
+            # password_reset.save()
+
+            # send the password reset email
+            path = reverse(
+                'password_reset_confirm',
+                args=[user_pk_to_url_str(user), temp_key],
+            )
+            url = build_absolute_uri(None, path) # PASS NONE INSTEAD OF REQUEST
+
+            context = {
+                'current_site': current_site,
+                'user': user,
+                'password_reset_url': url,
+                'request': request,
+            }
+            if app_settings.AUTHENTICATION_METHOD != app_settings.AuthenticationMethod.EMAIL:
+                context['username'] = user_username(user)
+            get_adapter(request).send_mail('account/email/password_reset_key',
+                                           email, context)
+        return self.cleaned_data['email']
+
+
+class CustomPasswordResetSerializer(PasswordResetSerializer):
+    @property
+    def password_reset_form_class(self):
+        return CustomAllAuthPasswordResetForm
+
