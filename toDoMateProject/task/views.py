74919fa396d410677539df84bbc47326c44a1d60
mathema123 <garam1732@snu.ac.kr>
2023-01-14 05:27:16 +0900

    add tag

--- toDoMateProject/task/views.py
+++ toDoMateProject/task/views.py
@@ -3,41 +3,37 @@ import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import generics
 
-from task.models import Task
-from task.serializers import TaskSerializer, TaskUpdateNameSerializer, TaskUpdateDateSerializer
+from task.models import Task, Tag
+from task.serializers import TaskUpdateNameSerializer, TaskUpdateDateSerializer, \
+    TagListCreateSerializer, TaskDetailDestroySerializer, TaskListCreateSerializer, TaskListSerializer
 
 
 # Create your views here.
 class TaskListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         uid = self.request.user.id
-        return Task.objects.filter(created_by_id=uid)
+        date = self.kwargs['date']
+        return Task.objects.filter(created_by_id=uid, date=date).order_by('tag')
         #return Task.objects.filter(created_by_id=uid, repeated=0) | Task.objects.filter(created_by_id=uid, repeated=1)
 
-    serializer_class = TaskSerializer
-    #permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        uid = self.request.user.id
-        serializer.save(created_by_id=uid)
-        #serializer.save(created_by_id=uid, repeated=0)
-
-
-class TaskListView(generics.ListCreateAPIView):
-    def get_queryset(self):
-        uid = self.request.user.id
-        date = self.kwargs['date']
-        return Task.objects.filter(created_by_id=uid, date=date)
-        #return Task.objects.filter(created_by_id=uid, date=date, repeated=0) | Task.objects.filter(created_by_id=uid, date=date, repeated=1)
-
-    serializer_class = TaskUpdateNameSerializer
+    serializer_class = TaskListCreateSerializer
     #permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         uid = self.request.user.id
         date = self.kwargs['date']
         serializer.save(created_by_id=uid, date=date)
-        #serializer.save(created_by_id=uid, date=date, repeated=0)
+        #serializer.save(created_by_id=uid, repeated=0)
+
+
+class TaskListView(generics.ListAPIView):
+    def get_queryset(self):
+        uid = self.request.user.id
+        return Task.objects.filter(created_by_id=uid).order_by('date', 'tag')
+        #return Task.objects.filter(created_by_id=uid, date=date, repeated=0) | Task.objects.filter(created_by_id=uid, date=date, repeated=1)
+
+    serializer_class = TaskListSerializer
+    #permission_classes = [IsAuthenticated]
 
 
 class TaskDetailDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -47,7 +43,7 @@ class TaskDetailDestroyView(generics.RetrieveUpdateDestroyAPIView):
         return get_object_or_404(Task, created_by_id=uid, id=tid)
         #return get_object_or_404(Task, created_by_id=uid, id=tid, repeated=0)
 
-    serializer_class = TaskSerializer
+    serializer_class = TaskDetailDestroySerializer
     #permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'delete']
 
@@ -78,7 +74,7 @@ def switch_complete(request, *args, **kwargs):
     task = get_object_or_404(Task, created_by_id=uid, id=tid)
     task.complete = not task.complete
     task.save()
-    return redirect(f"http://127.0.0.1:8000/task/{tid}") #실제 url
+    return redirect(f"http://127.0.0.1:8000/task/detail/{tid}") #실제 url
 
 
 def switch_tomorrow(request, *args, **kwargs):
@@ -87,4 +83,23 @@ def switch_tomorrow(request, *args, **kwargs):
     task = get_object_or_404(Task, created_by_id=uid, id=tid)
     task.date = task.date + datetime.timedelta(days=1)
     task.save()
-    return redirect(f"http://127.0.0.1:8000/task/{tid}")  # 실제 url
+    return redirect(f"http://127.0.0.1:8000/task/detail/{tid}")  # 실제 url
+
+
+class TagListCreateView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        uid = self.request.user.id
+        return Tag.objects.filter(created_by_id=uid)
+
+    serializer_class = TagListCreateSerializer
+
+    def perform_create(self, serializer):
+        uid = self.request.user.id
+        serializer.save(created_by_id=uid)
+
+# class RepeatListCreateView(generics.ListCreateAPIView):
+#     def get_queryset(self):
+#         uid = self.request.user.id
+#         return Repeat.objects.filter(created_by_id=uid)
+#
+#     serializer_class = RepeatListCreateSerializer
