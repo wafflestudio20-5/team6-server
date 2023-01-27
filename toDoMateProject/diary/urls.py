ab168159c5a32d9b3ab833f772c2aaf14a5d6d78
EC2 Default User <ec2-user@ip-172-31-10-192.ap-northeast-2.compute.internal>
2023-01-27 13:25:13 +0000

    url updated

--- toDoMateProject/diary/urls.py
+++ toDoMateProject/diary/urls.py
@@ -4,12 +4,12 @@ from diary.views import DiaryListView, diary_redirect, DiaryListCreateView, Diar
     CommentListCreateView, CommentRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('mydiary', DiaryListView.as_view()),
-    path('mydiary/<date>', diary_redirect),
-    path('mydiary/<date>/create', DiaryListCreateView.as_view()),
-    path('mydiary/<date>/update', DiaryRetrieveUpdateDeleteView.as_view()),
+    path('mydiary/', DiaryListView.as_view()),
+    path('mydiary/<date>/', diary_redirect),
+    path('mydiary/<date>/create/', DiaryListCreateView.as_view()),
+    path('mydiary/<date>/update/', DiaryRetrieveUpdateDeleteView.as_view()),
 
-    path('watch/<did>', DiaryWatchView.as_view()),
-    path('comment/<did>', CommentListCreateView.as_view()),
-    path('comment/detail/<cid>', CommentRetrieveUpdateDestroyView.as_view()),
+    path('watch/<did>/', DiaryWatchView.as_view()),
+    path('comment/<did>/', CommentListCreateView.as_view()),
+    path('comment/detail/<cid>/', CommentRetrieveUpdateDestroyView.as_view()),
 ]
