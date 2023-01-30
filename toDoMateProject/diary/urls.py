40432cd5a1eb7cf92081098a0ed666503cd7e702
mathema123 <garam1732@snu.ac.kr>
2023-01-30 17:25:37 +0900

    did type

--- toDoMateProject/diary/urls.py
+++ toDoMateProject/diary/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('mydiary/<date>/create/', DiaryListCreateView.as_view()),
     path('mydiary/<date>/update/', DiaryRetrieveUpdateDeleteView.as_view()),
 
-    path('watch/<did>/', DiaryWatchView.as_view()),
-    path('comment/<did>/', CommentListCreateView.as_view()),
-    path('comment/detail/<cid>/', CommentRetrieveUpdateDestroyView.as_view()),
+    path('watch/<int:did>/', DiaryWatchView.as_view()),
+    path('comment/<int:did>/', CommentListCreateView.as_view()),
+    path('comment/detail/<int:cid>/', CommentRetrieveUpdateDestroyView.as_view()),
     
     path('search/<uid>/', DiarySearchListView.as_view()),
     path('search/<uid>/<date>/', DiarySearchDateListView.as_view()),
