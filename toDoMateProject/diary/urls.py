585f8314e463bc210f1cf9202d9e8281e05ba057
mathema123 <garam1732@snu.ac.kr>
2023-02-02 22:35:48 +0900

    Remove slash

--- toDoMateProject/diary/urls.py
+++ toDoMateProject/diary/urls.py
@@ -4,15 +4,15 @@ from diary.views import DiaryListView, DiaryDateListView, DiaryListCreateView, D
     CommentListCreateView, CommentRetrieveUpdateDestroyView ,DiarySearchListView, DiarySearchDateListView
 
 urlpatterns = [
-    path('mydiary/', DiaryListView.as_view()),
-    path('mydiary/<date>/', DiaryDateListView.as_view()),
-    path('mydiary/<date>/create/', DiaryListCreateView.as_view()),
-    path('mydiary/<date>/update/', DiaryRetrieveUpdateDeleteView.as_view()),
+    path('mydiary', DiaryListView.as_view()),
+    path('mydiary/<date>', DiaryDateListView.as_view()),
+    path('mydiary/<date>/create', DiaryListCreateView.as_view()),
+    path('mydiary/<date>/update', DiaryRetrieveUpdateDeleteView.as_view()),
 
-    path('watch/<int:did>/', DiaryWatchView.as_view()),
-    path('comment/<int:did>/', CommentListCreateView.as_view()),
-    path('comment/detail/<int:cid>/', CommentRetrieveUpdateDestroyView.as_view()),
+    path('watch/<int:did>', DiaryWatchView.as_view()),
+    path('comment/<int:did>', CommentListCreateView.as_view()),
+    path('comment/detail/<int:cid>', CommentRetrieveUpdateDestroyView.as_view()),
     
-    path('search/<uid>/', DiarySearchListView.as_view()),
-    path('search/<uid>/<date>/', DiarySearchDateListView.as_view()),
+    path('search/<uid>', DiarySearchListView.as_view()),
+    path('search/<uid>/<date>', DiarySearchDateListView.as_view()),
 ]
